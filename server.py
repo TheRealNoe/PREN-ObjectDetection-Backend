5cab298b4dcdcd82a805eec3f2e10ed7229b651d
Noe Aufdenblatten <noe.aufdenblatten@stud.hslu.ch>
2023-04-23 22:23:30 +0200

    Server new return: boxes with pixel unit / score filter

--- server.py
+++ server.py
@@ -17,24 +17,12 @@ model = tf.saved_model.load('ssd_mobilenet_v2_fpnlite_640x640_coco17_tpu-8/saved
 @app.route('/detect', methods=['POST'])
 def predict():
     image = request.files['image']
-    image_np = np.fromstring(image.read(), dtype=np.uint8) 
+    image_np = np.frombuffer(image.read(), dtype=np.uint8) 
     img = cv2.imdecode(image_np, cv2.IMREAD_COLOR) 
     preds = run_inference_for_single_image(model, img)
     return pd.Series(preds).to_json(orient='values')
 
 def load_image_into_numpy_array(path):
-  """Load an image from file into a numpy array.
-
-  Puts image into numpy array to feed into tensorflow graph.
-  Note that by convention we put it into a numpy array with shape
-  (height, width, channels), where channels=3 for RGB.
-
-  Args:
-    path: a file path (this can be local or on colossus)
-
-  Returns:
-    uint8 numpy array with shape (img_height, img_width, 3)
-  """
   img_data = tf.io.gfile.GFile(path, 'rb').read()
   image = Image.open(BytesIO(img_data))
   (im_width, im_height) = image.size
@@ -51,28 +39,23 @@ def run_inference_for_single_image(model, image):
   model_fn = model.signatures['serving_default']
   output_dict = model_fn(input_tensor)
 
-  # All outputs are batches tensors.
-  # Convert to numpy arrays, and take index [0] to remove the batch dimension.
-  # We're only interested in the first num_detections.
-  num_detections = int(output_dict.pop('num_detections'))
-  output_dict = {key:value[0, :num_detections].numpy() 
-                 for key,value in output_dict.items()}
-  output_dict['num_detections'] = num_detections
+  # Extract the relevant information from the output dictionary
+  num_detections = int(output_dict['num_detections'][0])
+  classes = [int(output_dict['detection_classes'][0, i]) for i in range(num_detections)]
+  scores = [float(output_dict['detection_scores'][0, i]) for i in range(num_detections)]
+  boxes = [[int(output_dict['detection_boxes'][0, i, j] * image.shape[0]) for j in range(4)] for i in range(num_detections)]
 
-  # detection_classes should be ints.
-  output_dict['detection_classes'] = output_dict['detection_classes'].astype(np.int64)
-   
-  # Handle models with masks:
-  if 'detection_masks' in output_dict:
-    # Reframe the the bbox mask to the image size.
-    detection_masks_reframed = utils_ops.reframe_box_masks_to_image_masks(
-              output_dict['detection_masks'], output_dict['detection_boxes'],
-               image.shape[0], image.shape[1])      
-    detection_masks_reframed = tf.cast(detection_masks_reframed > 0.5,
-                                       tf.uint8)
-    output_dict['detection_masks_reframed'] = detection_masks_reframed.numpy()
-    
-  return output_dict
+  # Filter detections with score lower than 0.8
+  filtered_detections = []
+  for i in range(num_detections):
+      if scores[i] >= 0.8:
+          filtered_detections.append({
+              'class': classes[i],
+              'score': scores[i],
+              'box': boxes[i]
+          })
+
+  return filtered_detections
 
 if __name__ == '__main__':
     app.run(debug=True, port=443, host="0.0.0.0")
