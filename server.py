bf8dcd0b65e6142ca105fd02e18997ff7dfb4c0a
Noe Aufdenblatten <noe.aufdenblatten@stud.hslu.ch>
2023-05-23 16:39:21 +0200

    Remove unused function

--- server.py
+++ server.py
@@ -22,13 +22,6 @@ def predict():
     preds = run_inference_for_single_image(model, img)
     return pd.Series(preds).to_json(orient='values')
 
-def load_image_into_numpy_array(path):
-  img_data = tf.io.gfile.GFile(path, 'rb').read()
-  image = Image.open(BytesIO(img_data))
-  (im_width, im_height) = image.size
-  return np.array(image.getdata()).reshape(
-      (im_height, im_width, 3)).astype(np.uint8)
-
 def run_inference_for_single_image(model, image):
   # The input needs to be a tensor, convert it using `tf.convert_to_tensor`.
   input_tensor = tf.convert_to_tensor(image)
